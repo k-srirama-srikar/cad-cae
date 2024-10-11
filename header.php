76475907996ba0e1c9866ce98082ed96a16efee7
K Srirama Srikar <ksriramasrikar@gmail.com>
2024-10-11 17:24:41 +0530

    Added other files

---
+++ header.php
@@ -0,0 +1,43 @@
+<!-- <div class="container-fluid d-grid gap-3 align-items-center" style="grid-template-columns: 1fr 2fr;">
+      <div class="dropdown">
+        <a href="#" class="d-flex align-items-center col-lg-4 mb-2 mb-lg-0 link-body-emphasis text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+          <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"/></svg>
+        </a>
+        <ul class="dropdown-menu text-small shadow">
+          <li><a class="dropdown-item active" href="#" aria-current="page">Overview</a></li>
+          <li><a class="dropdown-item" href="#">Inventory</a></li>
+          <li><a class="dropdown-item" href="#">Customers</a></li>
+          <li><a class="dropdown-item" href="#">Products</a></li>
+          <li><hr class="dropdown-divider"></li>
+          <li><a class="dropdown-item" href="#">Reports</a></li>
+          <li><a class="dropdown-item" href="#">Analytics</a></li>
+        </ul>
+      </div> -->
+
+      <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome</title>
+    <!-- Link to favicon -->
+    <link rel="icon" href="src/assets/sri.jpg" type="image">
+    <!-- Link to stylesheets -->
+    <link rel="stylesheet" href="src/header.css">
+</head>
+<body>
+    <header>
+        <nav>
+            <div class="logo">
+                <a href="index.php"><img src="src/assets/sri.jpg" alt="Website Logo" class="logo-icon"></a>
+            </div>
+            <ul class="nav-links">
+                <li><a href="index.php">Home</a></li>
+                <li><a href="index.php/#about">About</a></li>
+                <li><a href="index.php/#services">Services</a></li>
+                <li><a href="index.php/#contact">Contact Us</a></li>
+            </ul>
+        </nav>
+    </header>
+</body>
+</html>
